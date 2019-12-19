416a4f0f2874284aa293a9d0a1d008c4179c853b
Sritha Kondragunta <kondraguntas8377@my.sduhsd.net>
2019-12-19 12:29:43 -0800

    .

--- variables.c
+++ variables.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-
 int main()
 {
-  int a;
-  int b = 3;
-  int c;
+ int a;
+ int b = 3;
+ int c;
+ int d;
 
-  a = 2;
-  c = a + b;
-  printf("Sum of %d and %d is %d\n", a, b, c);
+ a = 2;
+ c = a + b;
+ printf("Sum of %d and %d is %d\n", a,b,c);
+
+ d = a;
+ a = c;
+ c = d;
+
+ printf("Swapped a(2) and c(5); a=%d, c=%d\n", a,c);
+
 }
+
