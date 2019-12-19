27e96cf1437d4fd1525d0aecbbfbdf2acc78e822
Sritha Kondragunta <kondraguntas8377@my.sduhsd.net>
2019-12-19 12:42:15 -0800

    commit

--- sizeofvariables.c
+++ sizeofvariables.c
@@ -2,18 +2,6 @@
 
 int main()
 {
-  int a = 545;
-
-  // print value and size of an int variable
-  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
-}
-
-
-
-#include<stdio.h>
-
-int main()
-{
  int a = 545;
  char b = 'a';
  float c = 5.245;
