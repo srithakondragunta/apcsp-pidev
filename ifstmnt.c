f6e789818e1c89232ddfd7a42951203fc024e251
Sritha Kondragunta <kondraguntas8377@my.sduhsd.net>
2019-12-19 12:56:55 -0800

    4

---
+++ ifstmnt.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main()
+{
+ int a = 0;
+ int b = 1;
+
+ if (a == b)
+ {
+   printf("a is equal to b\n");
+ }
+ else
+ {
+   printf("a is not equal to b\n");
+ }
+ if (a != b)
+ {
+   printf("a is not equal to b\n");
+ }
+ else
+ {
+   printf("a is equal to b\n");
+ }
+ if (a > b)
+ {
+   printf("a is greater than b\n");
+ }
+ else
+ {
+   printf("a is less than b\n");
+ }
+ if (a < b)
+ {
+   printf("a is less than b\n");
+ }
+ else
+ {
+   printf("a is greater than b\n");
+ }
+ if (a >= b)
+ {
+   printf("a is greater than or equal to b b\n");
+ }
+ else
+ {
+   printf("a is less than b\n");
+ }
+ if (a == 0 && b == 0)
+ {
+   printf("a and b are equal to 0\n");
+ }
+ else
+ {
+   printf("a and b are not equal to 0\n");
+ }
+ if (a == 0 || b == 0)
+ {
+   printf("either a or b is equal to 0\n");
+ }
+ else
+ {
+   printf("both a and b are not equal to 0\n");
+ }
+ if (!(a == 0))
+ {
+   printf("a is not equal to 0\n");
+ }
+ else
+ {
+   printf("a is equal to 0\n");
+
+}
+}
